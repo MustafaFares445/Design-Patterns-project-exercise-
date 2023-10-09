1c5894c1fa0e9a5ed60a7f2d20818e88ef9af08c
Mustafa Fares <mustaphafares445@gmail.com>
2023-10-10 00:37:44 +0300

    Pool Pattern

---
+++ Creational/Pool/Car.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Creational\Pool;
+
+class Car
+{
+    private \DateTime $rentAt;
+
+    public function __construct()
+    {
+        $this->rentAt = new \DateTime();
+    }
+
+    public function move(): string
+    {
+        return "car is moving";
+    }
+}
